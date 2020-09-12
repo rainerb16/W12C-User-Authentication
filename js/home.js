be92b8dfaab11427b821eae8701d1eb076f7d7e5
Rainer Brieger <hello@rainerb.com>
2020-09-12 16:54:20 -0600

    Added log out button and log in button if there is no user logged in

--- js/home.js
+++ js/home.js
@@ -4,9 +4,36 @@ let userToken = Cookies.get("user-token");
 
 if(userLoggedIn == undefined) {
     document.getElementById("welcome-message").innerHTML = "No User logged in!";
+    let loginBtn = document.createElement("BUTTON");
+    loginBtn.innerHTML = "Log In";
+    document.getElementById("login-btn").appendChild(loginBtn);
 } else {
     document.getElementById("welcome-message").innerHTML = "<h2>Welcome </h2>" + "<h2>" + userLoggedIn + "</h2>";
+    let logoutBtn = document.createElement("BUTTON");
+    logoutBtn.innerHTML = "Log Out";
+    document.getElementById("logout-btn").appendChild(logoutBtn);
+    showColors();
+};
+
+
+// LOG IN FUNCTION WHEN NO USER LOGGED IN
+function noUser() {
+    window.open("../index.html", "_self");
 }
+
+document.getElementById("login-btn").addEventListener("click", noUser);
+
+
+// LOG OUT FUNCTION
+function logOut() {
+    window.open("../index.html", "_self");
+    Cookies.remove("user-session");
+    Cookies.remove("user-token");
+}
+
+document.getElementById("logout-btn").addEventListener("click", logOut);
+
+
 
 // GET COLORS ARRAY
 function showColors() {
@@ -21,10 +48,9 @@ function showColors() {
                   document.getElementById("post-container").innerHTML += "<div class='color-box'></div>";
                   colorBox[i].style.backgroundColor = showColors.data[i].color;
               }
-          }
+          };
       }
       ajax.open("GET", "https://reqres.in/api/unknown", true);
       ajax.setRequestHeader("Content-Type", "application/json");
       ajax.send();
 }
-showColors();
